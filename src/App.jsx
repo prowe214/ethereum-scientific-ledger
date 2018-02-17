1406ab7647816c2b3dc333da036e5e30de322b91
Paul Rowe <prowe214@gmail.com>
2018-02-17 11:09:47 -0700

    showing a list of studies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ListItem from "./ListItem/listItem";
 
@@ -13,21 +12,35 @@ class App extends Component {
     results: "results are here",
     discussion: "some discussion",
     acknowledgements: "acknowledgements talking about people",
-    citations: "citations of people"
+    citations: "citations of people",
+    publishDate: Date.now(),
+    id: 0,
+    reviewedStudyId: null,
+    isPeerReview: false,
+    author: "Jerry von Scientism"
+  }
+
+  studies = () => {
+    let result = [];
+    for (let i = 0; i < 10; i++) {
+      this.testobject.id = i;
+      result.push(this.testobject);
+    }
+    return result;
   }
 
   render() {
     console.log(this.testobject);
-    
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Journal</h1>
         </header>
         <div className="container">
-          <p className="App-intro">
-            <ListItem data={this.testobject} expanded="true"></ListItem>
-          </p>
+          {this.studies().map(article => 
+            <ListItem data={article} expanded={false} key={article.id}></ListItem>
+          )}
         </div>
       </div>
     );
--- src/ListItem/listItem.jsx
+++ src/ListItem/listItem.jsx
@@ -1,32 +1,75 @@
 import React, { Component } from 'react';
+import './listItem.css';
+import * as helpers from '../Helpers/helpers';
 
 class ListItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      // study: {
-      //   title: this.props.title,
-      //   abstract: this.props.abstract,
-      //   introduction: this.props.introduction,
-      //   methods: this.props.methods,
-      //   results: this.props.results,
-      //   discussion: this.props.discussion,
-      //   acknowledgements: this.props.acknowledgements,
-      //   citations: this.props.citations
-      // },
-      expanded: this.props.expanded
-    }
+  state = {
+    expanded: this.props.expanded
   }
+      // study: {
+      //   title
+      //   abstract
+      //   introduction
+      //   methods
+      //   results
+      //   discussion
+      //   acknowledgements
+      //   citations
+      //   author
+      //   date
+      //   isPeerReview
+      //   reviewedStudyId    <- for peer reviews only, null otherwise
+      // },
+      // expanded: this.props.expanded
 
   handleExpandoToggle = () => {
-    this.state.expanded = !this.state.expanded;
+    // this.state.expanded = !this.state.expanded;
+    const current = this.state.expanded;
+    this.setState({expanded: !current});
   }
 
   render() {
     return(
       <div className="list-item">
-        <div className="panel study-title">
-          <h3>{this.props.title}</h3>
+        <div className="panel study-title">{helpers.toTitleCase(this.props.data.title)}</div>
+        <div className="expander" onClick={this.handleExpandoToggle}><span>{this.state.expanded ? '-' : '+'}</span></div>
+        <div className={"details" + (this.state.expanded ? ' expanded' : '')}>
+          <div className="panel study-author">
+            <span className="section-name">Author</span>
+            {this.props.data.author}
+          </div>
+          <div className="panel study-date">
+            <span className="section-name">Published</span>
+            {this.props.data.publishDate}
+          </div>
+          <div className="panel study-abstract">
+            <span className="section-name">Abstract</span>
+            {this.props.data.abstract}
+          </div>
+          <div className="panel study-introduction">
+            <span className="section-name">Introduction</span>
+            {this.props.data.introduction}
+          </div>
+          <div className="panel study-methods">
+            <span className="section-name">Methods</span>
+            {this.props.data.methods}
+          </div>
+          <div className="panel study-results">
+            <span className="section-name">Results</span>
+            {this.props.data.results}
+          </div>
+          <div className="panel study-discussion">
+            <span className="section-name">Discussion</span>
+            {this.props.data.discussion}
+          </div>
+          <div className="panel study-acknowledgements">
+            <span className="section-name">Acknowledgements</span>
+            {this.props.data.acknowledgements}
+          </div>
+          <div className="panel study-citations">
+            <span className="section-name">Citations</span>
+            {this.props.data.citations}
+          </div>
         </div>
       </div>
     )
