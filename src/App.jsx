fb1f44754a92d996080bf8809afffcd2f040c978
Paul Rowe <prowe214@gmail.com>
2018-02-18 08:12:16 -0700

    review ui, filters, postnew

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,58 @@ import logo from './sledger-logo-1.svg';
 import List from './List/list';
 import PostNew from "./PostNew/postNew";
 import ReviewStudy from "./ReviewStudy/reviewStudy";
+import ViewStudy from "./ViewStudy/viewStudy";
 
 class App extends Component {
-  state = {
-    route: 'all'
+  constructor() {
+    super();
+    this.state = {
+      originalData: []
+    }
+  }
+  sampleobject = {
+    title: "test title",
+    abstract: "lots of words for an abstract",
+    introduction: "here is an intro",
+    methods: "methods are presented here",
+    results: "results are here",
+    discussion: "some discussion",
+    acknowledgements: "acknowledgements talking about people",
+    citations: "citations of people",
+    publishDate: Date.now(),
+    id: 0,
+    reviewedStudyId: null,
+    isPeerReview: false,
+    author: "Jerry von Scientism"
+  }
+
+  getAllStudies = () => {
+    let result = [];
+
+    for (let i = 0; i < 10; i++) {
+      let newObj = Object.assign({}, this.sampleobject);
+      newObj.id = i;
+
+      const rand = Math.floor(Math.random() * 100);
+
+      if (rand % 2 === 0) {
+        newObj.isPeerReview = true;
+        newObj.reviewedStudyId = Math.floor(Math.random() * 10);
+      }
+
+      result.push(newObj);
+    }
+    return result;
+  }
+
+  componentWillMount() {
+    this.setState({originalData: this.getAllStudies()});
   }
 
   render() {
-    // let view = null;
-    // const setView = (route) => {
-    //   if (route) {
-    //     this.setState({route: route});
-    //   } else {
-    //     this.setState({route: 'all'});
-    //   }
-
-    //   if (this.state.route === 'all') {
-    //     view = ( 
-    //       this.studies().map(study => 
-    //         <ListItem data={study} expanded={false} key={study.id}></ListItem>
-    //       )
-    //     )
-    //   } else if (this.state.route === 'post new') {
-    //     view = (<div>Hi!</div>)
-    //   }
-    // }
 
     return (
-      <div>
+      <div className="page-container">
         <header className="App-header">
           <img className="logo" src={logo} alt="logo"/>
           <div className="nav">
@@ -42,9 +66,13 @@ class App extends Component {
           </div>
         </header>
         <div className="content">
-          <Route exact path="/" component={List}/>
+          <Route exact path="/" render={(props) => { return <List allData={this.state.originalData} {...props} /> }}/>
+          <Route exact path="/review" render={(props) => { return <List allData={this.state.originalData} {...props} /> }}/>
           <Route path="/new" component={PostNew}/>
-          <Route path="/review" component={ReviewStudy}/>
+          <Route exact path="/review/:id" render={(props) => {return <ReviewStudy data={this.state.originalData[props.match.params.id]}/>}}/>
+          <Route 
+            path="/view/:id" 
+            render={(props) => { return <ViewStudy {...props}/> }}/>
         </div>
       </div>
     );
--- src/ReviewStudy/reviewStudy.jsx
+++ src/ReviewStudy/reviewStudy.jsx
@@ -1,12 +1,65 @@
 import React, { Component } from "react";
+// import { Link } from 'react-router-dom';
+import Moment from "moment";
+import '../ListItem/listItem.css';
 
 class ReviewStudy extends Component {
+  // NEEDS
+  // - Autopopulate with some info (title, id)
+  // - Button from original article
+  constructor() {
+    super();
+    this.state = {
+      form: {
+        title: '',
+        summary: '',
+        criticisms: '',
+        validatesStudy: false
+      }
+    }
+  }
+
+  componentDidMount() {
+    let state = this.state;
+    state.form.title = this.props.data.title;
+    this.setState(state);
+  }
 
   render(){
+    const date = Moment(this.props.data.publishDate).format('MMMM Do, YYYY').toString();
 
     return(
-      <div className="form review-study">
-        HELLO REVIEW STUDY
+      <div className="review-study">
+        <h2>Submit a Peer Review</h2>
+        <p>Add your peer review to verify the merit of this study.</p>
+
+        <div className="panel">
+          <h3>{this.props.data.title}</h3>
+          <div>Published on {date}</div>
+          <div className="abstract">
+            <div className="section-name">Original Abstract</div>
+            <div>{this.props.data.abstract}</div>
+          </div>
+          <div className="review-form">
+            <div className="section-name">Your Review</div>
+            <label htmlFor="summary">Summary
+              <textarea name="summary" onChange={this.handleChange}/>
+            </label>
+            <label htmlFor="criticisms">Criticisms
+              <textarea name="criticisms" onChange={this.handleChange}/>
+            </label>
+            <div className="label">Given your criticisms, do you believe that this study is validated?</div>
+              <div className="radio-input">
+                <input className="inline-block" name="validatesStudy" checked={this.state.form.validatesStudy === false} type="radio" onChange={this.handleChange} id="isPeerReviewFalse"/>
+                <label className="inline-block" htmlFor="validatesStudy">No, this study is not sufficient to consider its conclusions valid</label>
+              </div>
+              <div className="radio-input">
+                <input className="inline-block" name="validatesStudy" checked={this.state.form.validatesStudy === true} type="radio" onChange={this.handleChange} id="isPeerReviewTrue"/>
+                <label className="inline-block" htmlFor="validatesStudy">Yes, the conclusions of this study are valid</label>
+              </div>
+            </div>
+          <div className="btn submit">Submit</div>
+        </div>
       </div>
     )
   }
